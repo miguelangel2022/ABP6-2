822abf76c6e5d33ad3d20e7f9564d9c95805712c
miguelangel2022 <101416097+miguelangel2022@users.noreply.github.com>
2022-05-10 12:34:25 -0400

    10 Amy 2022

--- src/components/Tabla.vue
+++ src/components/Tabla.vue
@@ -1,73 +1,84 @@
 <template>
-<hr>
-<div class="titulo">
-  <h1>ABP 6.2</h1>
-</div>
-<hr>
-<div class="Pokemon">
-<div class="container fluid">
-<table class="table bg-info text-white">
-  <thead class="bg-primary">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Descripcion</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for = "(Personaje, index) in Personajes" :key="index">
-      <th>{{Personaje.id}}</th>
-      <td>{{Personaje.nombre}}</td>
-      <td>{{Personaje.descripcion}}</td>
-    </tr>
-    
-  </tbody>
-</table>
-</div>
-</div>
-
-
-  
+  <hr />
+  <div class="titulo">
+    <h1>ABP 6.2</h1>
+  </div>
+  <hr />
+  <input type="text" v-model="filtro" placeholder="Filtrar por Nombre" />
+  <hr />
+  <div class="Pokemon">
+    <div class="container fluid">
+      <table class="table bg-info text-white">
+        <thead class="bg-primary">
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Descripcion</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(Personaje, index) in filteredPersonajes" :key="index">
+            <th>{{ Personaje.id }}</th>
+            <td>{{ Personaje.nombre }}</td>
+            <td>{{ Personaje.descripcion }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Personajes",
   data: () => ({
-    Personajes :
-    [{
-      id: "1",
-      nombre: "picachu",
-      descripcion: "lorem ipsum uhiuhiuhihoi"
-    },
-    {
-      id: "2",
-      nombre: "kakuna",
-      descripcion: "lorem ipsum uhiuhiuhihoi"
-    },
-    {
-      id: "3",
-      nombre: "beedrill",
-      descripcion: "lorem ipsum uhiuhiuhihoi"
-    },
-    {
-      id: "4",
-      nombre: "metapod",
-      descripcion: "lorem ipsum uhiuhiuhihoi"
-    },
-    {
-      id: "5",
-      nombre: "rattata",
-      descripcion: "lorem ipsum uhiuhiuhihoi"
-    }
-    ]
-    
+    Personajes: [
+      {
+        id: "1",
+        nombre: "picachu",
+        descripcion: "lorem ipsum uhiuhiuhihoi",
+      },
+      {
+        id: "2",
+        nombre: "kakuna",
+        descripcion: "lorem ipsum uhiuhiuhihoi",
+      },
+      {
+        id: "3",
+        nombre: "beedrill",
+        descripcion: "lorem ipsum uhiuhiuhihoi",
+      },
+      {
+        id: "4",
+        nombre: "metapod",
+        descripcion: "lorem ipsum uhiuhiuhihoi",
+      },
+      {
+        id: "5",
+        nombre: "rattata",
+        descripcion: "lorem ipsum uhiuhiuhihoi",
+      },
+    ],
+
+    filtro: ''
   }),
+
+  computed:{
+    filteredPersonajes: function(){
+      return this.Personajes.filter((Personaje) => {
+        return Personaje.nombre.match(this.filtro);
+      })
+    }
+  }
 };
+
+
 </script>
 
 <style>
-.tabla{
-    background-color: rgb(196, 255, 214);
+.pokemon {
+  background-color: rgb(196, 255, 214);
 }
 </style>
+
+/* https://www.youtube.com/watch?v=G34_yNV8FMY */
